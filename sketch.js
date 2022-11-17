8afb67e4c9b2102d924a575314e6e6acd78bd13e
Matthew Greasley <info@electronica.org.uk>
2022-11-17 17:20:12 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+var song
+var fft
+
+function preload() {
+song = loadSound('amphitheatre.mp3')
+}
+
+function setup() {
+  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES)
+  fft = new p5.FFT()
+}
+
+function draw() {
+  // put drawing code here
+  background(0)
+  stroke(255)
+  //noFill()
+
+  translate(width / 2, height / 2)
+
+  var wave = fft.waveform()
+
+  beginShape()
+  for (var i = 0; i <=180; i++) {
+    var index = floor(map(i, 0, 180, 0, wave.length-1))
+     
+    var r = map(wave[index], -1, 1, 150, 350)
+
+    var x = r * sin(i)
+    var y = r * cos(i)
+    vertex(x, y)
+  }
+  endShape()
+  beginShape()
+  for (var i = 0; i <=180; i++) {
+    var index = floor(map(i, 0, 180, 0, wave.length-1))
+     
+    var r = map(wave[index], -1, 1, 150, 350)
+
+    var x = r * -sin(i)
+    var y = r * cos(i)
+    vertex(x, y)
+  }
+  endShape()
+}
+
+function mouseClicked() {
+if (song.isPlaying()){
+  song.pause()
+  noLoop()
+} else {
+  song.play()
+  loop()
+}
+}
